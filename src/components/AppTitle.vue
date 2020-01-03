23b6c0477447057c8b045bdbfc28c12eff93bc4d
Gregor Lennie <gregorlennie@hotmail.co.uk>
2020-01-03 12:14:05 +0000

    Adds AppTitle component to App.vue and passes in data

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-title',
+  props: ['title']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
